8120442c458e23150e2c4419a9abe571cfab6f94
Zach Morek <zmorek@gmail.com>
2012-06-07 23:09:03 -0400

    Created a sample application using gem

---
+++ Gemfile
@@ -0,0 +1,1 @@
+gem 'twitter_analyzer', :git => 'git://github.com/ZachBeta/twitter_analyzer.git'
---
+++ do_it.rb
@@ -0,0 +1,5 @@
+require 'twitter_analyzer'
+
+@a = TwitterAnalyzer.new('twitter')
+
+puts @a.words_most_used_desc
